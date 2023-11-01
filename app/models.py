3fd15aa5640fbc6c12953dd7969f40b3b637cd60
BaoNgan18 <nguyenlengan231526@gmail.com>
2023-11-01 16:49:03 +0700

    add mysql

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from app import db, app
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    products= relationship('product', backref='category', lazy=True)
+class Product(db.Model):
+    __tablename__ ="product"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    price = Column(Float, default=0)
+    img = Column(String(100), nullable=True)
+    category_id =Column(Integer, ForeignKey(Category.id), nullable=False)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        # c1 = Category(name="Mobile")
+        # c2 = Category(name="Tablet")
+        # db.session.add(c1)
+        # db.session.add(c2)
+        # db.session.commit()
+        db.create_all()
